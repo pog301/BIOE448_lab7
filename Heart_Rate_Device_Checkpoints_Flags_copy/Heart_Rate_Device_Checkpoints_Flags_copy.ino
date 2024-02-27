c0a45b1a7a38b1fe926a0e8d457d1a603ed239f2
pog301 <po8@rice.edu>
2024-02-27 14:49:05 -0600

    Initial commit - copying code from last week

---
+++ Heart_Rate_Device_Checkpoints_Flags_copy/Heart_Rate_Device_Checkpoints_Flags_copy.ino
@@ -0,0 +1,44 @@
+const int sensor_pin = A0;
+int pulse_signal = 0;
+float BPM = 0.0;
+
+float pulse_period = 0;
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+int upper_threshold = 850;
+int lower_threshold = 800;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pulse_signal = analogRead(sensor_pin);
+  //Serial.println(pulse_signal);
+  delay(50);
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+    } else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      //Serial.print(pulse_period);
+      BPM = 60000.0 / pulse_period;
+      Serial.println(BPM);
+      first_peak_detected = false;
+    }
+  }
+  if (pulse_signal < lower_threshold) {
+    any_peak_detected = false;
+  }
+
+}
+
+
